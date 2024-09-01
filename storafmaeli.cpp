f87a945936893cad8e98db1629f84e409a211f9c
Noah Beaudoin <150757909+Plettz@users.noreply.github.com>
2024-09-01 16:41:13 -0400

    Add files via upload

---
+++ storafmaeli.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    cin >> age;
+    
+    if ((age % 10) == 0)
+    {
+        cout << "Jebb";
+    }
+    else
+    {
+        cout << "Neibb";
+    }
+    
+    return 0;
+}
---
+++ storafmaeli.py
@@ -0,0 +1,7 @@
+age = input()
+age = int(age)
+
+if (age % 10) == 0:
+    print("Jebb")
+else:
+    print("Neibb")
